352fae45dc19866844720bf14edbdf2830c6c806
kunmi02 <mrkhunmie@gmail.com>
2021-02-21 01:54:39 +0100

    test rout commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :followings
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
