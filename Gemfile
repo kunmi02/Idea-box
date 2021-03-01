6ead28cf1c8649e3a4bf56d9452fd95e1b9fc8ff
kunmi02 <mrkhunmie@gmail.com>
2021-03-01 17:25:42 +0100

    add test

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.7.2'
 
 gem 'bulma-rails', '~> 0.6.1'
-gem 'simple_form'
+# gem 'simple_form'
 
 gem 'gravatar_image_tag', '~> 1.2'
 # Use bootstrap for styling and responsiveness
@@ -39,6 +39,8 @@ gem 'bootsnap', '>= 1.4.4', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rspec-rails'
+  # gem 'rspec-rails', github: 'rspec/rspec-rails', branch: 'rails-6-1-dev'
 end
 
 group :development do
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
   has_many :followings, class_name: 'Following', dependent: :destroy, foreign_key: :followed_id
   has_many :shared_ideas, class_name: 'Idea', dependent: :destroy, foreign_key: :author_id
 
-  validates :username, presence: true, length: { minimum: 5, maximum: 10 }
-  validates :email, presence: true, length: { minimum: 5, maximum: 30 }
-  validates :fullname, presence: true, length: { minimum: 5, maximum: 30 }
+  validates :username, presence: true, length: { minimum: 3, maximum: 10 }
+  validates :email, presence: true, length: { minimum: 3, maximum: 30 }
+  validates :fullname, presence: true, length: { minimum: 3, maximum: 30 }
   validates_uniqueness_of :username
   validates_uniqueness_of :email
 end
